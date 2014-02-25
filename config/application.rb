064f77fb46d8b4bb1bfd410665c2e66fbef72e0e
Anthony Capone <wnytechguy@gmail.com>
2014-02-25 00:38:14 -0300

    Do not require directories in application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path './config/db_connection.rb'
 
-Dir.entries( File.expand_path('./app/models'))[2..-1].each do |file|
+Dir.entries(File.expand_path('./app/models')).each do |file|
   path = File.expand_path("./app/models/#{file}")
-  require path if !file.to_s.empty? and File.exists? path
+  unless file.to_s.empty? || Dir.exists?(path) || !File.exists?(path)
+    require path
+  end
 end
