4ff6df63e2c076e920b59794a7cb77d16032adb5
Anthony Capone <wnytechguy@gmail.com>
2014-02-25 00:04:45 -0300

    Fixed pathing issue when requiring models

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require File.expand_path './config/db_connection.rb'
 
-Dir.entries(
-  File.expand_path('./app/models')
-)[2..-1].each { |file| require File.expand_path("./app/models/#{file}") }
+Dir.entries( File.expand_path('./app/models'))[2..-1].each do |file|
+  path = File.expand_path("./app/models/#{file}")
+  require path if File.exists? path
+end
